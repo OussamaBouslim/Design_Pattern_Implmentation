220ea78644aad2e7e7d04fa94902db9ecc27ada0
OUSSAMA BOUSLIM <oussama.bouslim1602@gmail.com>
2023-12-30 13:06:05 +0100

    implementation d'exercice1

---
+++ src/designpattern/Client.java
@@ -0,0 +1,15 @@
+package designpattern;
+
+/**
+ * Client class to demonstrate the use of ProgrammeInterface implementations.
+ */
+public class Client {
+    public static void main(String[] args) {
+       int programNumber = 4;
+
+        ProgrammeInterface program = ProgrammeFactory.getProgramme(programNumber);
+
+        // Now, you can call the 'go' method on the selected program.
+        program.go();
+    }
+}
---
+++ src/designpattern/Programme4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpattern;
+
+/**
+ *
+ * @author H-R
+ */
+public class Programme4 implements ProgrammeInterface {
+    public Programme4(){
+        
+    }
+     public void go(){
+        System.out.println("Je suis le traitement 4");
+    }
+}
