fc46c57856f87627ebe3a2c25db86bf8efd01595
OUSSAMA BOUSLIM <oussama.bouslim1602@gmail.com>
2023-12-30 15:51:54 +0100

    implementation d'exercice6

--- src/exercice6/Observateur.java
+++ src/exercice6/Observateur.java
@@ -3,14 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package exercice5;
+package exercice6;
 
 /**
  *
  * @author H-R
  */
-public interface Noeud {
+public interface Observateur {
     
-    void accept(VisiteurArbre visiteur);
-    
+        void actualise();
 }
---
+++ src/exercice6/Sujet.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercice6;
+
+/**
+ *
+ * @author H-R
+ */
+import java.util.*;
+
+
+public abstract class Sujet
+{
+    protected List<Observateur> observateurs = new ArrayList<Observateur>();   
+ 
+    public void ajoute(Observateur observateur)
+ {
+    observateurs.add(observateur);
+ }
+    public void retire(Observateur observateur)
+ {
+    observateurs.remove(observateur);
+ }
+    public void notifie()
+    {
+        for (Observateur observateur: observateurs)
+        observateur.actualise();
+    }
+}
